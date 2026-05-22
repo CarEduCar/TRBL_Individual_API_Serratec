ad8cb148c413dbf6b3fd4d9e8f138d9ccef3056d
CarEduCar <carloscarvalho201532@gmail.com>
2026-05-22 00:03:53 -0300

    feat: criacao da configuracao basica inicial do Controller

--- TRBL_Individual_API/src/main/java/org/serratec/TRBL_Individual_API/Controller/UsuarioController.java
+++ TRBL_Individual_API/src/main/java/org/serratec/TRBL_Individual_API/Controller/UsuarioController.java
@@ -1,5 +1,13 @@
 package org.serratec.TRBL_Individual_API.Controller;
 
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Validated
+@RequestMapping("/SOSCursos")
 public class UsuarioController {
+	
 
 }
